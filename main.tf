0bfb9155097da04de5776c6ea0309fa1881483e2
Iamneo <techadmin@iamneo.ai>
2023-09-22 15:12:56 +0000

    Backup Commit

--- main.tf
+++ main.tf
@@ -11,4 +11,11 @@ provider "aws" {
     access-key="AKIAV4PYHDF5BK6ZRNFH"
     screct-key="r7cMUisYe0LrH3zaQf1b1SYjVoeFqQhqBu7EC2E2"
 }
-resource "aws instance" "U"
+resource "aws instance" "Ubuntu Server 20.04"{
+    ami="ami-0d52744d6551d851e"
+    instance-type="t2.micro"
+}
+output "public-ip"
+{
+    value=aws-instance.demo-instance.public.ip
+}
---
+++ variable.tf
@@ -0,0 +1,8 @@
+variable "ami" {
+  type        = string
+  default     = "ami-0d52744d6551d851e"
+}
+variable "instance-type" {
+  type        = string
+  default     = ""
+}
